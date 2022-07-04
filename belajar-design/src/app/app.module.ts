8d53d661bd9cf0ff6b414db36ff360a074547410
Rayhan Junicko <rjunicko@gmail.com>
2022-07-04 11:26:46 +0700

    Belajar Design
    figma by arrayid (The Sugiharto's)
    FirstPage

---
+++ belajar-design/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
+import { ProfileComponent } from './content-profile/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
