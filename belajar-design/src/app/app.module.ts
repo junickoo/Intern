fc852c7dc1101f2047d71ca2080a2419768030e4
Rayhan Junicko <rjunicko@gmail.com>
2022-07-05 10:05:45 +0700

    full-router-link

--- belajar-design/src/app/app.module.ts
+++ belajar-design/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,18 +6,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
 import { ProfileComponent } from './content-profile/profile/profile.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ProfileComponent
+    ProfileComponent,
+    OverviewPageComponent,
+    ProjectComponent,
+    SkillsComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: ProfileComponent,
+      },
+      {
+        path: 'overview',
+        component: OverviewPageComponent,
+      },
+      {
+        path: 'project',
+        component: ProjectComponent,
+      },
+      {
+        path: 'skills',
+        component: SkillsComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
